b6c12f46ea887799496ee8cd3662b29bc05f783c
kieudoannam <kieunam2808@gmail.com>
2023-02-16 23:05:31 +0700

    makeId-func & something

---
+++ makeid-func.js
@@ -0,0 +1,9 @@
+makeId = (number) => {
+    let text = "";
+    let possible = "ABCDEFGHIKLMNOPQRSTYWZ0123456789";
+
+    for (let i = 0; i < number; i++ )
+        text+=possible.charAt(Math.floor(Math.random() * possible.length));
+        
+    return text;
+};
